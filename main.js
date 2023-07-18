9618ea443b48a272e33b8a1be69ad1ac08499305
MhdTarhini <mohamedtarhini95@gmail.com>
2023-07-19 02:53:41 +0300

    add button html and css +  addToExpenseList()

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-addToEXpenseList(text,amount){
-    return `<ul>
+function addToExpenseList(text, amount) {
+  return `<ul class="expense-item">
     <li> ${text}</li>
-    <li> ${amount}`
+    <li> ${amount}</li>
+    <li> <button class="delete">DELETE<button></li>
+    </ul> `;
 }
 
-
-
 $(document).ready(function () {
-  
+  let inputName = $("#expense-name").val();
 });
