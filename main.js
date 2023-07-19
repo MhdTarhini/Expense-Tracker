d12aed25d865042fe2996e52e6b68071b8f29f9f
MhdTarhini <mohamedtarhini95@gmail.com>
2023-07-19 13:44:36 +0300

    clean the code

--- main.js
+++ main.js
@@ -11,9 +11,6 @@ function AddToExpenseList() {
   let input_amount = $("#expense-amount");
   const expense_list = $(".expense-list");
 
-  if ((expense_list.lenght = 0)) {
-    expense_list.append(expenseItem);
-  }
   if (input_name.val().trim() === "") {
     input_name.addClass("not-valid");
     return;
@@ -28,20 +25,16 @@ function AddToExpenseList() {
   }
   const expense_item = $(ExpenseListItem(input_name.val(), input_amount.val()));
 
-  // totalPrice = parseInt($(".total-amount").text());
   total_price += parseInt(input_amount.val());
   $(".total-amount").text(total_price);
 
   expense_item.find(".delete-expense").click(function () {
     expense_item.remove();
     let item_price = parseInt(expense_item.find(".item-amount").text());
-
     total_price -= item_price;
     $(".total-amount").text(total_price);
-    return total_price;
   });
 
-  // $(".total-amount").text = totalPrice;
   expense_list.append(expense_item);
   input_name.val("");
   input_amount.val("");
