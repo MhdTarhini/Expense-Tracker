5de15ce61070c0d55b3177e831a97ed1ae2e50cb
MhdTarhini <mohamedtarhini95@gmail.com>
2023-07-19 03:16:49 +0300

    add expense name and amount to list + add delete function

--- main.js
+++ main.js
@@ -1,13 +1,36 @@
-function addToExpenseList(text, amount) {
+function ExpenseListItem(text, amount) {
   return `<li>
     <span> ${text}</span>
     <span> ${amount}</span>
-    <button class="delete">DELETE<button>
+    <button class="delete-expense">DELETE<button>
     </li>`;
 }
 
+function AddToExpenseList() {
+  let inputName = $("#expense-name");
+  let inputAmount = $("#expense-amount");
+  const expenseList = $(".expense-list");
+
+  if (inputName.val().trim() === "") {
+    inputName.css("border-color", "red");
+    return;
+    return;
+  } else if (inputAmount.val().trim() === "") {
+    inputAmount.css("border-color", "red");
+    return;
+  }
+  const expenseItem = $(ExpenseListItem(inputName.val(), inputAmount.val()));
+
+  expenseItem.find(".delete-expense").click(function () {
+    expenseItem.remove();
+  });
+
+  expenseList.append(expenseItem);
+  inputName.val("");
+}
+
 $(document).ready(function () {
-  let inputName = $("#expense-name").val();
-  let inputAmount = $("#expense-amount").val();
-  let expenseList = $(".expense-list");
+  const addButton = $(".add-expense");
+
+  addButton.click(AddToExpenseList);
 });
