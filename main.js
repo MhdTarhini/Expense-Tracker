520b7574c6675d1cbb49a97ab4ad1e29ece1b954
MhdTarhini <mohamedtarhini95@gmail.com>
2023-07-19 13:42:28 +0300

    add total-price + check validation + add .not-valid class

--- main.js
+++ main.js
@@ -1,39 +1,54 @@
 function ExpenseListItem(text, amount) {
   return `<li>
-    <p>name: ${text}</p>
-    <p> anmount : ${amount}</p>
+    <p>${text}</p>
+    <p class="item-amount">${amount}</p>$
     <button class="delete-expense">DELETE</button>
     </li>`;
 }
-
+let total_price = 0;
 function AddToExpenseList() {
-  let inputName = $("#expense-name");
-  let inputAmount = $("#expense-amount");
-  const expenseList = $(".expense-list");
+  let input_name = $("#expense-name");
+  let input_amount = $("#expense-amount");
+  const expense_list = $(".expense-list");
 
-  if ((expenseList.lenght = 0)) {
-    expenseList.append(expenseItem);
+  if ((expense_list.lenght = 0)) {
+    expense_list.append(expenseItem);
   }
-  if (inputName.val().trim() === "") {
-    inputName.css("border-color", "red");
+  if (input_name.val().trim() === "") {
+    input_name.addClass("not-valid");
     return;
-  } else if (inputAmount.val().trim() === "") {
-    inputAmount.css("border-color", "red");
-    return;
+  } else {
+    input_name.removeClass("not-valid");
   }
-  const expenseItem = $(ExpenseListItem(inputName.val(), inputAmount.val()));
-  $(".total-amount").text += inputAmount.val;
-  expenseItem.find(".delete-expense").click(function () {
-    expenseItem.remove();
+  if (input_amount.val().trim() === "" || isNaN(input_amount.val())) {
+    input_amount.addClass("not-valid");
+    return;
+  } else {
+    input_amount.removeClass("not-valid");
+  }
+  const expense_item = $(ExpenseListItem(input_name.val(), input_amount.val()));
+
+  // totalPrice = parseInt($(".total-amount").text());
+  total_price += parseInt(input_amount.val());
+  $(".total-amount").text(total_price);
+
+  expense_item.find(".delete-expense").click(function () {
+    expense_item.remove();
+    let item_price = parseInt(expense_item.find(".item-amount").text());
+
+    total_price -= item_price;
+    $(".total-amount").text(total_price);
+    return total_price;
   });
 
-  expenseList.append(expenseItem);
-  inputName.val("");
-  inputAmount.val("");
+  // $(".total-amount").text = totalPrice;
+  expense_list.append(expense_item);
+  input_name.val("");
+  input_amount.val("");
 }
 
 $(document).ready(function () {
-  const addButton = $(".add-expense");
+  const add_button = $(".add-expense");
 
-  addButton.click(AddToExpenseList);
+  add_button.click(AddToExpenseList);
 });
