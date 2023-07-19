13032308569061f56cd8393c4f9f46a1320b7527
MhdTarhini <mohamedtarhini95@gmail.com>
2023-07-19 03:46:51 +0300

    add total expense

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 function ExpenseListItem(text, amount) {
   return `<li>
-    <span> ${text}</span>
-    <span> ${amount}</span>
-    <button class="delete-expense">DELETE<button>
+    <p>name: ${text}</p>
+    <p> anmount : ${amount}</p>
+    <button class="delete-expense">DELETE</button>
     </li>`;
 }
 
@@ -11,22 +11,25 @@ function AddToExpenseList() {
   let inputAmount = $("#expense-amount");
   const expenseList = $(".expense-list");
 
+  if ((expenseList.lenght = 0)) {
+    expenseList.append(expenseItem);
+  }
   if (inputName.val().trim() === "") {
     inputName.css("border-color", "red");
-    return;
     return;
   } else if (inputAmount.val().trim() === "") {
     inputAmount.css("border-color", "red");
     return;
   }
   const expenseItem = $(ExpenseListItem(inputName.val(), inputAmount.val()));
-
+  $(".total-amount").text += inputAmount.val;
   expenseItem.find(".delete-expense").click(function () {
     expenseItem.remove();
   });
 
   expenseList.append(expenseItem);
   inputName.val("");
+  inputAmount.val("");
 }
 
 $(document).ready(function () {
